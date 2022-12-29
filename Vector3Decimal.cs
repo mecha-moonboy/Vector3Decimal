0d350210e86fecf19172057aca91b305e5d8459a
mecha_moonboy <42070776+mecha-moonboy@users.noreply.github.com>
2022-12-29 09:15:31 -0500

    More flexibility
    Added another multiply and division operator, added `sqrMagnitue` and allowed `(Vector3)` cast syntax.

--- Vector3Decimal.cs
+++ Vector3Decimal.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using DecimalMath
+using UnityEngine;
+using DecimalMath;
 
 [System.Serializable]
 public struct Vector3Decimal
@@ -9,19 +9,28 @@ public struct Vector3Decimal
     public decimal z;
 
     public decimal magnitude;
-
+    public decimal sqrMagnitude;
+    public Vector3Decimal normalized {
+        get {
+            return this / magnitude;
+        }
+    }
     public Vector3Decimal(decimal x, decimal y, decimal z)
     {
         this.x = x;
         this.y = y;
         this.z = z;
 
-        decimal a = (x * x + y * y + z * z);
+        this.sqrMagnitude = (x * x + y * y + z * z);
 
-        if ((float)a == 0f)
-            magnitude = 0;
-        else
-            magnitude = DecimalEx.Sqrt(a);
+        if ((float)this.sqrMagnitude == 0f)
+    {
+        this.magnitude = 0;
+    }
+    else
+    {
+        this.magnitude = DecimalEx.Sqrt(sqrMagnitude);
+    }
     }
 
     public Vector3Decimal(Vector3 v3)
@@ -30,12 +39,16 @@ public struct Vector3Decimal
         y = (decimal)v3.y;
         z = (decimal)v3.z;
 
-        decimal a = (x * x + y * y + z * z);
+        this.sqrMagnitude = (x * x + y * y + z * z);
 
-        if ((float)a == 0f)
-            magnitude = 0;
-        else
-            magnitude = DecimalEx.Sqrt(a);
+        if ((float)this.sqrMagnitude == 0f)
+    {
+        this.magnitude = 0;
+    }
+    else
+    {
+        this.magnitude = DecimalEx.Sqrt(sqrMagnitude);
+    }
     }
 
     public Vector3Decimal(float x, float y, float z)
@@ -44,12 +57,16 @@ public struct Vector3Decimal
         this.y = (decimal)y;
         this.z = (decimal)z;
 
-        decimal a = (decimal)(x * x + y * y + z * z);
+        this.sqrMagnitude = (decimal)(x * x + y * y + z * z);
 
-        if ((float)a == 0f)
-            magnitude = 0;
+        if ((float)this.sqrMagnitude == 0f)
+        {
+            this.magnitude = 0;
+        }
         else
-            magnitude = DecimalEx.Sqrt(a);
+        {
+            this.magnitude = DecimalEx.Sqrt(sqrMagnitude);
+        }
     }
     
 
@@ -57,18 +74,25 @@ public struct Vector3Decimal
     public static Vector3Decimal forward = new Vector3Decimal(0f, 0f, 1f);
     public static Vector3Decimal right = new Vector3Decimal(1f, 0f, 0f);
     public static Vector3Decimal zero = new Vector3Decimal(0f, 0f, 0f);
-
+    
+    
     public Vector3 ToVector3f()
     {
         Vector3 newV3 = new Vector3((float)x, (float)y, (float)z);
         return newV3;
     }
-    public static float Distance(Vector3Decimal a, Vector3Decimal b)
+    
+    public static explicit operator Vector3(Vector3Decimal v)
     {
-        float dist;
+        return v.ToVector3f();
+    }
+    
+    public static decimal Distance(Vector3Decimal a, Vector3Decimal b)
+    {
+        decimal dist;
 
         Vector3Decimal diff = a - b;
-        dist = (float)diff.magnitude;
+        dist = diff.magnitude;
 
         return dist;
 
@@ -110,12 +134,25 @@ public struct Vector3Decimal
         Vector3Decimal prod = new Vector3Decimal(a.x * f, a.y * f, a.z * f);
         return prod;
     }
-
+    
+    public static Vector3Decimal operator *(decimal f, Vector3Decimal a)
+    {
+        Vector3Decimal prod = new Vector3Decimal(a.x * f, a.y * f, a.z * f);
+        return prod;
+    }
+    
     public static Vector3Decimal operator /(Vector3Decimal a, Vector3Decimal b)
     {
         Vector3Decimal quotient = new Vector3Decimal(a.x / b.x, a.y / b.y, a.z / b.z);
         return quotient;
     }
+
+    public static Vector3Decimal operator /(Vector3Decimal a, decimal b)
+    {
+        Vector3Decimal quotient = new Vector3Decimal(a.x / b, a.y / b, a.z / b);
+        return quotient;
+    }
+
 
     public static Vector3Decimal Sqrts(Vector3Decimal vec)
     {
